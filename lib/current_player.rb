32ca189bdda79dca00529eabf9811cb040796f9a
Susan Rolfes <rolfes57069@yahoo.com>
2018-08-17 15:10:11 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count (board)
+  turn = 0
+  board.each do |marker|
+  if marker == "X" || marker == "O"
+    turn += 1
+   end
+  end
+  return turn
+end
+
+def current_player (board)
+ whoturn = turn % 2
+ if whoturn.even? 
+   return "X"
+ else
+   return "O"
+end
